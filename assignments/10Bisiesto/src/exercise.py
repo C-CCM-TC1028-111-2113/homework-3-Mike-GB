18711eab033db4d6b3f0b97259fd313751bfe127
Mike-GB <88850952+Mike-GB@users.noreply.github.com>
2021-08-29 16:53:21 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,7 +1,19 @@
-
+def bisiesto(a):
+    if a % 4 == 0 :
+        if a % 100 == 0 :
+            if a % 400 == 0 :
+                print("True")
+            else :
+                print("False")     
+        else :
+            print("True")
+    else :
+        print("False")
+        
 def main():
-    #escribe tu código abajo de esta línea
-    pass
-
+  #escribe tu código abajo de esta línea
+    a=int(input(""))
+    bisiesto(a)
+    
 if __name__=='__main__':
     main()
