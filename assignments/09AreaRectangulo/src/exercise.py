54ec8289ab1ac8d09a7e7de125ee9e1ffe60ff08
Mike-GB <88850952+Mike-GB@users.noreply.github.com>
2021-08-29 16:52:12 -0500

    Update exercise.py

--- assignments/09AreaRectangulo/src/exercise.py
+++ assignments/09AreaRectangulo/src/exercise.py
@@ -1,7 +1,12 @@
-
+def area(b, h):
+    a=b*h
+    print("El área del rectángulo es:",a)
+    
 def main():
-    #escribe tu código abajo de esta línea
-    pass
-
+  #escribe tu código abajo de esta línea
+    b=float(input("Dame la base: "))
+    h=float(input("Dame la altura: "))
+    area(b, h)
+    
 if __name__=='__main__':
     main()
